789b2bf09f39aec31b41db202325c20891728bb2
mondar88 <52878910+mondar88@users.noreply.github.com>
2021-04-15 01:30:31 +0530

    feat: input and output function created

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var inputTxt = document.querySelector("#input-text");
 var buttonTranslate = document.querySelector("#btnTrans");
 var outputTxt = document.querySelector("#output");
-
+//input
 function displayfunction(){
     console.log("now it is clicked!");
     console.log(inputTxt.value);
+    outputis();
+}
+
+
+//output
+function outputis(){
+    outputTxt.textContent = inputTxt.value;
 }
 buttonTranslate.addEventListener("click", displayfunction);
