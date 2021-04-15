e8a713cfa72187d4f86345894534599920e0ae5b
mondar88 <52878910+mondar88@users.noreply.github.com>
2021-04-15 09:54:44 +0530

    feat: added error handler

--- script.js
+++ script.js
@@ -10,17 +10,27 @@ function displayfunction(){
 
 //processing
 function getURL(extension){
-    var header = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"+"?"+"text="+extension;
+    var apiUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+    var header = apiUrl+"?"+"text="+extension;
     console.log(header);
     return header;
 
 }
+
+//error handling
+function errorHandler(error){
+    console.log("we encountered some error!", error);
+    alert("Seems server is down! come back after a while.")
+}
+
 //output
 function outputis(urlHead){
     fetch(urlHead)
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(function writeToOutput(json){
+        outputTxt.textContent = json.contents.translated;
+    })
+    .catch(errorHandler)
 }
-
 
 buttonTranslate.addEventListener("click", displayfunction);
